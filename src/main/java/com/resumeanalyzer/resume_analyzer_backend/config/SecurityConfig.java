49392fc6a647546d100543efecad7f79013a3776
Ishva Kubendiran <ishvakubendiran@gmail.com>
2026-06-28 16:42:34 +0530

    Fix CORS for Vercel deployment

--- src/main/java/com/resumeanalyzer/resume_analyzer_backend/config/SecurityConfig.java
+++ src/main/java/com/resumeanalyzer/resume_analyzer_backend/config/SecurityConfig.java
@@ -51,7 +51,8 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowedOrigins(List.of("http://localhost:5173",
-            "https://your-vercel-app.vercel.app"));
+    "https://ai-resume-analyzer-flame-gamma.vercel.app"));
+            
         configuration.setAllowedMethods(List.of("GET","POST","PUT","DELETE","OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
